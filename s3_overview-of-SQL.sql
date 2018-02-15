090a0b573f32113ab454095c64b828afa681221f
Mark Beveridge <mark@dreamtime.org>
2018-02-15 22:21:17 +0000

    s3 SQL queries

---
+++ s3_overview-of-SQL.sql
@@ -0,0 +1,30 @@
+SELECT propclass
+FROM parcels                   -- parcels of land
+WHERE acres > 44
+
+--
+
+SELECT *
+INTO qlayer                    -- new table, to display as a separate layer in QGIS
+FROM parcels
+WHERE acres > 44
+
+--
+
+DROP TABLE qlayer;
+
+SELECT parcels.* into qlayer   -- clear the 'qlayer' table and create new query
+FROM parcels, firm             -- FIRM is "Flood Insurance Rate Map" ('flood zone map')
+WHERE st_intersects(parcels.geometry,firm.geometry) -- a spatial function()
+AND firm.zone = 'X'            -- parcels that intersect the FIRM in zones designated 'X'
+
+--
+
+DROP TABLE qlayer;
+
+SELECT parcels.* into qlayer
+FROM parcels, firm, hydro                              -- hydro has the rivers & streams
+WHERE st_intersects(parcels.geometry,firm.geometry)
+AND firm.zone = 'AE'                                   -- zones designated 'AE'
+AND parcels.asmt > 500000                              -- $assessment value of parcel
+AND st_distance(parcels.geometry,hydro.geometry) < 600 -- parcels <600ft from hydrology
