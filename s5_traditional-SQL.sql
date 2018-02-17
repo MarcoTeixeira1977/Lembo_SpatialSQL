8015bb4d79c51eead9bd7c14d63cc800d913e096
Mark Beveridge <mark@dreamtime.org>
2018-02-17 17:18:25 +0000

    s5 SQL queries cont'd

--- s5_traditional-SQL.sql
+++ s5_traditional-SQL.sql
@@ -17,7 +17,7 @@ WHERE
 
 SELECT parcels.swis, propclas.description
 FROM parcels, propclas
-WHERE parcels.propclass::text = propclas.value
+WHERE parcels.propclass::text = propclas.value -- or parcels.propclass = propclas.value::integer
 
 --
 
@@ -26,6 +26,31 @@ FROM parcels
 RIGHT JOIN propclas ON
 parcels.propclass = propclas.value::numeric
 
+--# GROUP BY
+
+SELECT sum(acres) AS sumacres, propclass
+FROM parcels
+GROUP BY propclass
+
 --
+
+SELECT sum(acres) AS sumacres, left(propclass::text,1)
+FROM parcels
+GROUP BY left(propclass::text,1)
+
+--
+
+SELECT count(*) AS NumProps, propclass
+FROM parcels
+GROUP BY propclass
+
+--
+
+SELECT sum(asmt) sumacres, propclass
+FROM parcels, firm
+WHERE propclass > 200 AND propclass < 300
+AND st_contains(firm.geometry, parcels.geometry)
+AND firm.zone = 'X'
+GROUP BY propclass
 
 
