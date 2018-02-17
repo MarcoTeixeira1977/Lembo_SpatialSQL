438596a2e2ad6b8f567f7b724400994099f48677
Mark Beveridge <mark@dreamtime.org>
2018-02-17 23:34:16 +0000

    s5 SQL queries cont'd

--- s5_traditional-SQL.sql
+++ s5_traditional-SQL.sql
@@ -26,17 +26,24 @@ FROM parcels
 RIGHT JOIN propclas ON
 parcels.propclass = propclas.value::numeric
 
---# GROUP BY
+--# GROUP BY. (Performs aggregate computations on data, and groups them by one or more columns)
 
-SELECT sum(acres) AS sumacres, propclass
-FROM parcels
-GROUP BY propclass
+SELECT acres, propclass FROM parcels
 
 --
 
-SELECT sum(acres) AS sumacres, left(propclass::text,1)
+SELECT sum(acres) AS sumacres, propclass       -- How many acres are in each property class?
 FROM parcels
-GROUP BY left(propclass::text,1)
+GROUP BY propclass       -- 'If you're going to use an aggregate function, with a 2nd column,
+                         -- ...you're going to have to use the GROUP BY'
+
+--
+
+SELECT sum(acres) AS sumacres, sum(asmt)::numeric::money AS sumasmt,
+		avg(asmt)::numeric::money AS avgasmt, propclass
+FROM parcels
+GROUP BY propclass
+ORDER BY propclass
 
 --
 
@@ -52,5 +59,16 @@ WHERE propclass > 200 AND propclass < 300
 AND st_contains(firm.geometry, parcels.geometry)
 AND firm.zone = 'X'
 GROUP BY propclass
+
+--
+
+SELECT sum(acres) AS sumacres, left(propclass::text,1)
+FROM parcels
+GROUP BY left(propclass::text,1)
+
+--
+
+
+
 
 
