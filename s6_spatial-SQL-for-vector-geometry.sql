2f7a0f281a8385e66148a2f41eac914bd7ed6cba
Mark Beveridge <mark@dreamtime.org>
2018-02-23 23:17:35 +0000

    s6 SQL queries cont'd

--- s6_spatial-SQL-for-vector-geometry.sql
+++ s6_spatial-SQL-for-vector-geometry.sql
@@ -4,25 +4,26 @@
 -- SRID is a Spatial Reference system IDentifier
 
 SELECT ST_SRID(geom) FROM states2;                     -- what is current projection ('geom' field)
-                                                       -- ...'0' would mean 'it doesn't know'
+                                                       -- ...'0' means 'don't know' (where in space)
 
 --
 
-SELECT UpdateGeometrySRID('states2','geometry',2796)   -- define a projection (the ref system)
+SELECT UpdateGeometrySRID('states2','geom',2796)   -- define a projection (the ref system)
                                                        -- ...doesn't transform the actual geometry
+                                                       -- (layer name, field name, new SRID)
 
 --
 
-SELECT ST_Transform(geometry,3450) FROM states;        -- change a projection
-SELECT ST_Transform(geometry,2796) FROM states;
+SELECT ST_Transform(geom,3450) FROM states;            -- change a projection
+SELECT ST_Transform(geom,2796) FROM states;
 
 
-SELECT name, ST_Transform(geometry,3450) AS geometry INTO states3
+SELECT name, ST_Transform(geom,3450) AS geometry INTO states3
 FROM states2;
 
 
 
-ALTER TABLE states2
-	ALTER COLUMN geometry
+ALTER TABLE states2                                    -- no need to create new table (cf. 'states3')
+	ALTER COLUMN geom
 	TYPE Geometry(Multipolygon,2959)
-	USING ST_Transform(geometry,2959);                  -- convert 'geometry' col (to value '2959')
+	USING ST_Transform(geom,2959);                     -- convert 'geom' col (to value '2959')
