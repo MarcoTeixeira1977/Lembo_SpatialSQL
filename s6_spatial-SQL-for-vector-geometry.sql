efc5de9357ce16ca53828de8a6e3b3fd68d88b65
Mark Beveridge <mark@dreamtime.org>
2018-02-23 23:53:24 +0000

    s6 SQL queries cont'd

--- s6_spatial-SQL-for-vector-geometry.sql
+++ s6_spatial-SQL-for-vector-geometry.sql
@@ -8,22 +8,22 @@ SELECT ST_SRID(geom) FROM states2;                     -- what is current projec
 
 --
 
-SELECT UpdateGeometrySRID('states2','geom',2796)   -- define a projection (the ref system)
+SELECT UpdateGeometrySRID('states2','geom',2796)       -- define a projection (the ref system)
                                                        -- ...doesn't transform the actual geometry
                                                        -- (layer name, field name, new SRID)
 
 --
 
-SELECT ST_Transform(geom,3450) FROM states;            -- change a projection
-SELECT ST_Transform(geom,2796) FROM states;
+SELECT ST_Transform(geom,3450) FROM states2;           -- change projection (but not perm in table)
 
+--
 
-SELECT name, ST_Transform(geom,3450) AS geometry INTO states3
+SELECT name, ST_Transform(geom,3450) AS geometry INTO states3  -- change table to see in QGIS layer
 FROM states2;
 
+--
 
-
-ALTER TABLE states2                                    -- no need to create new table (cf. 'states3')
+ALTER TABLE states2                   -- change existing table/layer, not create new (cf. 'states3')
 	ALTER COLUMN geom
 	TYPE Geometry(Multipolygon,2959)
-	USING ST_Transform(geom,2959);                     -- convert 'geom' col (to value '2959')
+	USING ST_Transform(geom,2959);    -- convert 'geom' col (to value '2959')
