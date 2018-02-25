983b20aa4c526258341075ab287e1a0613b6a4de
Mark Beveridge <mark@dreamtime.org>
2018-02-25 16:30:28 +0000

    s6 SQL queries cont'd

--- s6_spatial-SQL-for-vector-geometry.sql
+++ s6_spatial-SQL-for-vector-geometry.sql
@@ -14,11 +14,11 @@ SELECT UpdateGeometrySRID('states2','geom',2796)       -- define a projection (t
 
 --
 
-SELECT ST_Transform(geom,3450) FROM states2;           -- change projection (but not perm in table)
+SELECT ST_transform(geom,3450) FROM states2;           -- change projection (but not perm in table)
 
 --
 
-SELECT name, ST_Transform(geom,3450) AS geometry INTO states3  -- change table to see in QGIS layer
+SELECT name, ST_transform(geom,3450) AS geometry INTO states3  -- change table to see in QGIS layer
 FROM states2;
 
 --
@@ -26,11 +26,11 @@ FROM states2;
 ALTER TABLE states2                   -- change existing table/layer, not create new (cf. 'states3')
 	ALTER COLUMN geom
 	TYPE Geometry(Multipolygon,2959)
-	USING ST_Transform(geom,2959);    -- convert 'geom' col (to value '2959')
+	USING ST_transform(geom,2959);    -- convert 'geom' col (to value '2959')
 
 
 
---# Spatial operations. (Adjacent, Buffer, Contains, Distance, Intersect, more...)
+--# Spatial operations. (Adjacent, Buffer, Contains, Distance, Intersect)
 
 --http://postgis.net/docs/manual-1.3/ch06.html
 --http://postgis.org/docs/reference.html
@@ -38,7 +38,7 @@ ALTER TABLE states2                   -- change existing table/layer, not create
 --Adjacent :
 
 SELECT * FROM parcels
-WHERE st_touches(parcels.geometry,
+WHERE ST_touches(parcels.geometry,
 	(SELECT geometry
 	FROM parcels
 	WHERE parcelkey = '50070006200000010150000000')     -- Parcels touching specific one. (9 rows)
@@ -49,7 +49,7 @@ WHERE st_touches(parcels.geometry,
 DROP TABLE qlayer
 
 SELECT * INTO qlayer FROM parcels
-WHERE st_touches(parcels.geometry,
+WHERE ST_touches(parcels.geometry,
 	(SELECT geometry
 	FROM parcels
 	WHERE parcelkey = '50070006200000010150000000')
@@ -59,7 +59,7 @@ WHERE st_touches(parcels.geometry,
 
 SELECT sum(asmt)::numeric::money AS sumasmt, sum(land)::numeric::money AS sumland
 FROM parcels
-WHERE st_touches(parcels.geometry,
+WHERE ST_touches(parcels.geometry,
 	(SELECT geometry
 	FROM parcels
 	WHERE parcelkey = '50070006200000010150000000')    -- get value of adjacent assessments/land
@@ -69,7 +69,7 @@ WHERE st_touches(parcels.geometry,
 
 SELECT addrno || ' ' || addrstreet AS address
 FROM parcels
-WHERE st_touches(parcels.geometry,
+WHERE ST_touches(parcels.geometry,
 	(SELECT geometry
 	FROM parcels
 	WHERE parcelkey = '50070006200000010150000000')    -- addresses of adjacent parcels
@@ -81,7 +81,7 @@ WHERE st_touches(parcels.geometry,
 
 SELECT addrno || ' ' || addrstreet AS address
 FROM parcels
-WHERE st_touches(parcels.geometry,
+WHERE ST_touches(parcels.geometry,
 	(SELECT geometry
 	FROM parcels
 	WHERE parcelkey = '50070006200000010150000000')
@@ -92,7 +92,7 @@ AND asmt > 210000                                      -- 'find rich neighbours'
 
 DROP TABLE qlayer;
 
-SELECT parcels.parcel_id, st_buffer(geometry,100) AS geometry
+SELECT parcels.parcel_id, ST_buffer(geometry,100) AS geometry
 INTO qlayer
 FROM parcels
 WHERE parcelkey = '50070000900000040130000000'
@@ -104,7 +104,7 @@ DROP TABLE qlayer;
 SELECT parcels.*
 INTO qlayer
 FROM parcels,firm
-WHERE st_contains(firm.geometry,parcels.geometry)
+WHERE ST_contains(firm.geometry,parcels.geometry)
 AND firm.zone = 'AE'                           -- parcel is fully-contained inside flood zone 'AE' 
                                                -- ...107 rows
 
@@ -115,7 +115,7 @@ DROP TABLE qlayer;
 SELECT parcels.*
 INTO qlayer
 FROM parcels,firm
-WHERE st_intersects(firm.geometry,parcels.geometry) -- cf. 'Contains' [to help understanding that]
+WHERE ST_intersects(firm.geometry,parcels.geometry) -- cf. 'Contains' [to help understanding that]
 AND firm.zone = 'AE'                                -- parcel fully or partly in flood zone 'AE' 
                                                     -- ...320 rows
 
@@ -123,7 +123,7 @@ AND firm.zone = 'AE'                                -- parcel fully or partly in
 
 SELECT sum(asmt)::numeric::money, left(propclass::text,1) AS pc
 FROM parcels,firm
-WHERE st_contains(firm.geometry,parcels.geometry)
+WHERE ST_contains(firm.geometry,parcels.geometry)
 GROUP BY pc                                         -- 9 rows (0-9, but no '1')
 
 
@@ -138,12 +138,52 @@ FROM upstate
 --ST_Collect() turns a set of geometries (polygons/points/lines) into a collection,
 --so it becomes a multi-part object
 DROP TABLE qlayer;
-SELECT ST_ConvexHull(ST_Collect(geometry)) AS geometry -- 4-sided shape, other 2 points inside it
+SELECT ST_ConvexHull(ST_collect(geometry)) AS geometry -- 4-sided shape, other 2 points inside it
 INTO qlayer
 FROM upstate
 
 --Distance :
 
+SELECT ST_distance(parcels.geometry,firm.geometry)::integer as dist, parcels.parcel_id
+FROM parcels, firm
+WHERE ST_distance(parcels.geometry,firm.geometry) < 300     -- parcel and flood map < 300ft apart
+AND zone = 'X'                                              -- 7468 rows
+
+--
+
+SELECT ST_distance(parcels.geometry,firm.geometry)::integer as dist, parcels.parcel_id
+FROM parcels, firm
+WHERE ST_DWithin(parcels.geometry,firm.geometry,300)        -- (uses indexes, much faster)
+AND zone = 'X'                                              -- 7468 rows
+
+--
+
+SELECT sum(asmt)::numeric::money
+FROM parcels, firm
+WHERE ST_DWithin(parcels.geometry,firm.geometry,300)
+AND zone = 'X'                                              -- 1 row, total sum
+
+--Intersects :
+
+DROP TABLE qlayer;
+SELECT parcels.geometry AS g, parcels.parcelkey             -- whole parcels
+INTO qlayer
+FROM parcels, firm
+WHERE ST_intersects(parcels.geometry,firm.geometry)
+AND firm.zone = 'AE'                                        -- 320 rows
+
+--
+
+DROP TABLE qlayer;
+SELECT ST_intersection(parcels.geometry,firm.geometry) AS geometry, parcels.parcelkey  -- cuts parcels
+INTO qlayer
+FROM parcels, firm
+WHERE ST_intersects(parcels.geometry,firm.geometry)
+AND firm.zone = 'AE'                                        -- 320 rows
+
+
+
+--# Spatial operations: Topological Overlay. (ERASE, INTERSECT, IDENTITY)
 
 
 
